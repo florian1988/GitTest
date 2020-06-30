783b710e4a804d3da5fbcbc48e2db3fb2acf5e7d
florian <flo.moerzinger@gmx.at>
2020-06-30 13:12:33 +0200

    basic florian

---
+++ src/FStringB4.java
@@ -0,0 +1,9 @@
+public class FStringB4 {
+    public static void main(String[] args) {
+        String original = "Java is great!";
+
+        System.out.println("The character at position 1 is: " + original.charAt(1));
+        System.out.print("The character at  position 3 is: " + original.charAt(3));
+
+    }
+}
---
+++ src/Hanna2.java
@@ -0,0 +1,8 @@
+public class Hanna2 {
+    public static void main(String[] args) {
+        String hanna = "Did Hannah see bees? Hannah did";
+        System.out.println(hanna.charAt(12));
+    }
+}
+
+// outcome: e

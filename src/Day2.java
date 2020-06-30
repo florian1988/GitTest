79d5bf2db64cef5f9d4aac2261dc1810aff374d9
jimmy <jimmyjaytscheina@gmail.com>
2020-06-30 18:45:57 +0200

    exercise A31

--- src/Day2.java
+++ src/Day2.java
@@ -9,9 +9,12 @@ public class Day2 {
 
         System.out.println (hannah.indexOf('b'));
 
+        String original = "Java is great!";
 
+        System.out.println("The character at position 1 is: " + original.charAt(1));
+        System.out.println("The character at  position 3 is: " + original.charAt(3));
 //A2
-        String str = new String ("Was it a car or a cat i saw");
+        String str = "Was it a car or a cat i saw";
         System.out.println (str.substring(9, 12));
 
     }

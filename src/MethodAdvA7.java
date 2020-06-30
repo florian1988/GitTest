cea9a6d80bb20b95b9df9e9e2cf876fbe428f78e
jimmy <jimmyjaytscheina@gmail.com>
2020-06-30 23:04:28 +0200

    meanCalculate-method A7

--- src/MethodAdvA7.java
+++ src/MethodAdvA7.java
@@ -1,13 +1,13 @@
 public class MethodAdvA7 {
-    static String meanCalculator(int nr1, int nr2, int nr3){ // the 3 int Parameters
+    static String meanCalculator(int nr1, int nr2, int nr3){ //the 3 int Parameters
 
-        int average = (nr1+nr2+nr3) / 3; //calculation mean
+        int average = (nr1+nr2+nr3) / 3;               //calculation mean
 
-        return "The average value is: " + average; // returns text and mean
+        return "The average value is: " + average;    //returns text and mean
     }
 
-    public static void main(String[] args) {       //using the method with the needed values
-        System.out.print(meanCalculator(25,45,65));
+    public static void main(String[] args) {
+        System.out.print(meanCalculator(25,45,65));   //using the method with the needed values
     }
 }
 //The average value is: 45

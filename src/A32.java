ffeaa7a017bdadeb61c733cfd69f4a34e1ccb5a4
jimmy <jimmyjaytscheina@gmail.com>
2020-06-30 19:11:19 +0200

    exercise A32

---
+++ src/A32.java
@@ -0,0 +1,32 @@
+public class A32 {
+
+//A3.2
+    public static void main(String args[])
+    {
+        String s1 = "Java is juicy";
+
+        // prints true
+        System.out.println(s1.contains("is"));
+
+        // prints false
+        System.out.println(s1.contains("si"));
+
+//A3.3
+        String s2 = "Coding is fun";
+        String s4 = "un";
+        if (s2.contains(s4)) {
+            System.out.println(true);
+        }   else {
+            System.out.println(false);
+        }
+
+
+
+
+
+    }
+
+
+
+
+}

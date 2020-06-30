b858d796c3a2c9e97367a152601108c931f9a1dc
jimmy <jimmyjaytscheina@gmail.com>
2020-06-30 11:52:20 +0200

    exercise Test

--- src/Day2.java
+++ src/Day2.java
@@ -1,2 +1,19 @@
 public class Day2 {
+    public static void main(String[] args) {
+        String hannah = "Did Hannah see bees? Hannah did";
+
+//A1
+        System.out.println (hannah.length());
+
+        System.out.println (hannah.charAt(12));
+
+        System.out.println (hannah.indexOf('b'));
+
+
+//A2
+        String str = new String ("Was it a car or a cat i saw");
+        System.out.println (str.substring(9, 12));
+
+    }
 }
+

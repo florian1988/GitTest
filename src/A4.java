a1d96a7bcaef8e72b8ac1c94e49a083c08af4a8d
jimmy <jimmyjaytscheina@gmail.com>
2020-06-30 19:02:49 +0200

    exercise A3/4

---
+++ src/A4.java
@@ -0,0 +1,8 @@
+public class A4 {
+    public static void main(String[] args) {
+
+        String str = "Christian Sander";
+        String[] arrOfStr = str.split(" ");
+        System.out.println ("This is " + arrOfStr[0].charAt(0) + arrOfStr[1].charAt(0));
+    }
+}
